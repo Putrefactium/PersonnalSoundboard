f88911ef9642fd0c8a44ed2b649ba59fc0d17290
Jérémie <jeremie_marie14@hotmail.fr>
2024-06-11 23:47:34 +0200

    Ajout d'un fond, d'une barre volume, optimisation du responsive, ajout banniere, ajout bouton HAN stylisé

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
     var audioPlayer = document.getElementById('audioPlayer');
     var audioButtons = document.querySelectorAll('.audio-button');
+    var volumeControl = document.getElementById('volumeControl');
+    var volumeImage = document.getElementById('volumeImage');
+    var volumeContainer = document.querySelector('.volume-container');
 
     audioButtons.forEach(function(button) {
         button.addEventListener('click', function() {
@@ -9,4 +12,35 @@ document.addEventListener('DOMContentLoaded', function() {
             audioPlayer.play();
         });
     });
+
+    volumeImage.addEventListener('click', function() {
+        if (audioPlayer.volume === 0) {
+            audioPlayer.volume = 1; // Rétablir le volume
+            volumeControl.classList.remove('muted');
+        } else {
+            audioPlayer.volume = 0; // Mettre le volume à zéro
+            volumeControl.classList.add('muted');
+        }
+        toggleVolumeControlVisibility(); // Appeler la fonction pour gérer la visibilité
+    });
+
+    volumeControl.addEventListener('input', function() {
+        audioPlayer.volume = this.value;
+        if (this.value > 0) {
+            volumeControl.classList.remove('muted');
+        } else {
+            volumeControl.classList.add('muted');
+        }
+        toggleVolumeControlVisibility(); // Appeler la fonction pour gérer la visibilité
+    });
+
+    function toggleVolumeControlVisibility() {
+        if (volumeControl.classList.contains('muted')) {
+            volumeControl.classList.add('hidden'); // Ajouter la classe hidden
+            volumeImage.src = "muted.png"; // Changer l'image en muted.png
+        } else {
+            volumeControl.classList.remove('hidden'); // Retirer la classe hidden
+            volumeImage.src = "volume.png"; // Changer l'image en volume.png
+        }
+    }
 });
